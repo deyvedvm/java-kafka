54db9e83b4fb056e099df9b8834e0845304e7795
Deyve Vieira <deyvedvm@hotmail.com>
2019-11-03 21:17:33 -0300

    Kafka: Producer and Consumer

---
+++ src/main/java/dev/deyve/kafka/consumers/Consumer.java
@@ -0,0 +1,48 @@
+package dev.deyve.kafka.consumers;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Consumer.class.getName());
+
+        String BOOTSTRAP_SERVERS = System.getenv("BOOTSTRAP_SERVERS");
+        String GROUP_ID = System.getenv("GROUP_ID");
+        String TOPIC = System.getenv("TOPIC");
+
+        // create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // subscribe consumer to our topic(s)
+        consumer.subscribe(Arrays.asList(TOPIC));
+
+        // poll for new data
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100)); // new in Kafka 2.0.0
+
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("Key: " + record.key() + ", Value: " + record.value());
+                logger.info("Partition: " + record.partition() + ", Offset:" + record.offset());
+            }
+        }
+    }
+}
