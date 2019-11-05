203080f1ca5043f698f00b77728cf2d630f46bc6
Deyve Vieira <deyvedvm@hotmail.com>
2019-11-05 00:48:46 -0300

    Kafka: List Info and Delete Topic

--- run.sh
+++ run.sh
@@ -46,3 +46,13 @@ bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-fac
 # We can now see that topic if we run the list topic command
 
 bin/kafka-topics.sh --list --bootstrap-server localhost:9092
+
+# We can describe informations of topic
+
+bin/kafka-topics.sh --describe --bootstrap-server localhost:9092
+
+bin/kafka-topics.sh --describe --topic first_topic --bootstrap-server localhost:9092
+
+# Delete topic
+
+bin/kafka-topics.sh --delete --topic second_topic --bootstrap-server localhost:9092
